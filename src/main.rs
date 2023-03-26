d89e3bfbb9e237c54505897d1d76094465f5cf94
Ameen Al-Kaisi <ameen.alkaisi01@gmail.com>
2023-03-26 10:45:39 -0400

    finished making it scrape all the useful links

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use select::{document::Document, predicate};
-use std::collections::{HashSet, VecDeque};
+use std::collections::HashSet;
 
 // note: thirtyfour is selenium driver, so that is an alternative
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut next_wikis: VecDeque<String> = VecDeque::new();
-    next_wikis.push_back(
-        "https://wiki.borderconnect.com/index.php/Welcome_to_BorderConnect%27s_Support_Wiki"
-            .to_string(),
-    );
+    let mut next_wikis: Vec<String> = Vec::new();
+
+    // note, if need ot make general then look into way to not have constants into the strings
+    let base_url: String = String::from("https://wiki.borderconnect.com");
+
+    next_wikis.push("/index.php".to_owned());
 
     let mut visited: HashSet<String> = HashSet::new();
 
@@ -18,25 +19,46 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // exit on depth = 0 or all links visited
 
     while !next_wikis.is_empty() {
-        let cur_wiki = next_wikis.pop_front().unwrap();
+        let cur_wiki = next_wikis.pop().unwrap();
         if visited.contains(&cur_wiki) {
             continue;
         }
 
-        let resp = reqwest::get(&cur_wiki).await?.text().await?;
+        println!("{}", base_url.clone() + &cur_wiki);
+        let resp = reqwest::get(base_url.clone() + &cur_wiki)
+            .await?
+            .text()
+            .await?;
 
         visited.insert(cur_wiki);
 
         let dom = Document::from(resp.as_str());
-        for node in dom.find(predicate::Text) {
-            if node.html().chars().any(|c| !c.is_whitespace()) {
-                let cur_str: String = node.html().chars().filter(|c| !c.is_whitespace()).collect();
+        // gets first matched node of #mw-content-text and unwraps it
+        println!(
+            "{}",
+            dom.find(predicate::Attr("id", "mw-content-text"))
+                .next()
+                .unwrap()
+                .text()
+                .trim()
+        );
 
-                println!("{}", cur_str);
-            }
-        }
+        // only get relative links of href attributes of a links under
+        // the "Help Guides" page
 
-        todo!("figure out a way to display all the info properly here");
+        let someiter = dom
+            .find(predicate::Attr("id", "p-Help_Guides"))
+            .next() // going to the only result here
+            .unwrap()
+            .clone()
+            .find(predicate::Name("a")); // find all "a" tags below this node
+
+        let links: Vec<String> = someiter
+            .map(|node| node.clone().attr("href").unwrap().to_string())
+            .filter(|node| node.starts_with('/'))
+            .collect();
+
+        next_wikis.extend(links);
     }
 
     Ok(())
