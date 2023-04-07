43a2ac465dddc8eebbb375c25f89ec12f2387c22
Ameen Al-Kaisi <ameen.alkaisi01@gmail.com>
2023-04-06 21:26:17 -0400

    properly parses the links, but contains lots of duplicates

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use select::{document::Document, predicate};
+use scraper::{Html, Selector};
 use std::collections::HashSet;
 
 // note: thirtyfour is selenium driver, so that is an alternative
@@ -7,11 +7,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut next_wikis: Vec<String> = Vec::new();
 
     // note, if need ot make general then look into way to not have constants into the strings
-    let base_url: String = String::from("https://wiki.borderconnect.com");
+    let base_url = String::from("https://wiki.borderconnect.com");
 
     next_wikis.push("/index.php".to_owned());
 
     let mut visited: HashSet<String> = HashSet::new();
+
+    let help_guide_links_selector = Selector::parse("#p-Help_Guides a").unwrap();
 
     // collect next ones to visit in this run
     // finish this run first
@@ -30,23 +32,32 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             .text()
             .await?;
 
+        let dom = Html::parse_fragment(&resp);
+
         visited.insert(cur_wiki);
 
-        let dom = Document::from(resp.as_str());
+        // let dom = Document::from(resp.as_str());
         // gets first matched node of #mw-content-text and unwraps it
         println!(
             "{}",
+            /*
             dom.find(predicate::Attr("id", "mw-content-text"))
-                .next()
-                .unwrap()
-                .text()
-                .trim()
+               .next()
+               .unwrap()
+               .text()
+               .trim() */
+            dom.select(
+                &Selector::parse("#mw-content-text :not(h1):not(.references):not(.toc)").unwrap()
+            )
+            .map(|node| node.text().next().unwrap_or("").to_string())
+            .reduce(|accum, item| accum + &item + "\n")
+            .unwrap(),
         );
 
         // only get relative links of href attributes of a links under
         // the "Help Guides" page
 
-        let someiter = dom
+        /* let someiter = dom
             .find(predicate::Attr("id", "p-Help_Guides"))
             .next() // going to the only result here
             .unwrap()
@@ -57,9 +68,41 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             .map(|node| node.clone().attr("href").unwrap().to_string())
             .filter(|node| node.starts_with('/'))
             .collect();
+        */
+
+        let links = dom
+            .select(&help_guide_links_selector)
+            .map(|node| node.value().attr("href").unwrap().to_string())
+            .filter(|node| node.starts_with('/'));
 
         next_wikis.extend(links);
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use scraper::{Html, Selector};
+
+    #[test]
+    fn test_select() -> Result<(), Box<dyn std::error::Error>> {
+        let input = reqwest::blocking::get("https://www.google.com")
+            .unwrap()
+            .text()
+            .unwrap()
+            .to_string();
+
+        let dom = Html::parse_fragment(&input);
+
+        println!(
+            "{}",
+            dom.select(&Selector::parse("html :not(h1):not(.references):not(.toc)").unwrap())
+                .map(|node| node.text().next().unwrap_or("").to_string())
+                .reduce(|accum, item| accum + &item + "\n")
+                .unwrap()
+        );
+
+        Ok(())
+    }
 }
